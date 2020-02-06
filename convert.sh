392991f4cc638d933b2b50d4b7cfeb3490fe4dd5
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2020-02-06 14:58:16 -0300

    Parameterize conversion script

--- convert.sh
+++ convert.sh
@@ -1,2 +1,4 @@
-python3 torch2tf.py --input ~/Downloads/models/epoch055_0.04755_4.1284.pth --output age-model --model-class model:get_model --input-shape 1,3,224,224
+model_path=$1
+
+python3 torch2tf.py --input "$model_path"--output age-model --model-class model:get_model --input-shape 1,3,224,224
 
