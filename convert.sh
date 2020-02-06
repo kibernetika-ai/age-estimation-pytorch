2a4d7883b50135be107e901a10ca89ba74d77b36
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2020-02-06 15:00:05 -0300

    Fix convert script

--- convert.sh
+++ convert.sh
@@ -1,4 +1,4 @@
 model_path=$1
 
-python3 torch2tf.py --input "$model_path"--output age-model --model-class model:get_model --input-shape 1,3,224,224
+python3 torch2tf.py --input "$model_path" --output age-model --model-class model:get_model --input-shape 1,3,224,224
 
